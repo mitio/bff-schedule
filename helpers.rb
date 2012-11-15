c58dbbd7ee272132003d370f0fc4dfaa4b684d39
Dimitar Dimitrov <me@ddimitrov.name>
2012-11-15 04:59:53 +0200

    WIP on rendering
    Getting closer, bit by bit
    I'm soooooooo sleeeepy

--- helpers.rb
+++ helpers.rb
@@ -11,3 +11,27 @@ def weekday_for(date)
     'Неделя',
   ][date.wday - 1]
 end
+
+def combine_events_in_boxes(pdf, events, hour_height, min_event_height)
+  combined_events = []
+  last_combined_event = []
+
+  events.each do |event|
+    if event.coffee_break?
+      combined_events << last_combined_event if last_combined_event.any?
+      last_combined_event = []
+      combined_events << [event]
+    else
+      last_combined_event << event
+      combined_events_height = last_combined_event.map { |e| e.duration / 3600.0 * hour_height }.reduce(:+)
+
+      if combined_events_height >= min_event_height
+        combined_events << last_combined_event
+        last_combined_event = []
+      end
+    end
+  end
+  combined_events << last_combined_event if last_combined_event.any?
+
+  combined_events
+end
